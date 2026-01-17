164cf28d5a264b546dbb8044d6e6ae4d2608eed0
Wizari <wizaripost@gmail.com>
2026-01-17 16:31:14 +0300

    first commit

---
+++ src/main/kotlin/com/gmail/wizaripost/vk_team_bot/VkTeamBotApplication.kt
@@ -0,0 +1,11 @@
+package com.gmail.wizaripost.vk_team_bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkTeamBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<VkTeamBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmail/wizaripost/vk_team_bot/VkTeamBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmail.wizaripost.vk_team_bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkTeamBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
